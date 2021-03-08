1d6c5683f6b4366cca8f90118eaf2c23f8e4c313
mubeenghauri <ghauri.mubeen@gmail.com>
2021-03-08 15:14:58 -0600

    Fixes in production.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Schema\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // https://stackoverflow.com/questions/23786359/laravel-migration-unique-key-is-too-long-even-if-specified
+	Builder::defaultStringLength(191);
+
     }
 }
