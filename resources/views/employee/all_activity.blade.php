8d5ada8b007770f8d6d5972974f034b64b0ce3b6
Haris-Noori <harisnoori20@gmail.com>
2021-05-12 01:37:57 +0500

    Date column added in Employee All Activities view table

--- resources/views/employee/all_activity.blade.php
+++ resources/views/employee/all_activity.blade.php
@@ -4,7 +4,7 @@
         <!-- Page Heading -->
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">All Activities</h1>
-            <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+            {{-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a> --}}
         </div>
     
         <div class="col-md-12">
@@ -16,6 +16,7 @@
                         <th>Activity Description</th>
                         <th>Start Time</th>
                         <th>End Time</th>
+                        <th>Date</th>
                         <th>Edit Activity</th>
                         <th>Delete Activity</th>
                     </tr>
@@ -28,6 +29,7 @@
                                 <td> {{ $activity->activity_description }} </td>
                                 <td> {{ $activity->from }} </td>
                                 <td> {{ $activity->to }} </td>
+                                <td> {{ $activity->created_at }} </td>
                                 <td> <a href="#"><button class="btn btn-primary">Edit</button></a> </td>
                                 <td> <a href=""><button class="btn btn-danger">Remove</button></a> </td>
                             </tr>
